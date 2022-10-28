d3b1164ba8f98edcd4e6e026fc2e627f51264d76
Daniel Oyola <danieloyola@softerdo.com>
2022-10-28 00:11:38 -0400

    Added participats routes

---
+++ src/participants/participants.router.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const passport = require('passport')
+const participantsServices = require('./participants.services')
+require('../middlewares/auth.middleware')(passport)
+
+//Root route
+router.route('/:conversation_id/participants', participantsServices)
+  .get(passport.authenticate('jwt', {session: false}, participantsServices.getParticipantsByConversation))
+
+router.route('/:conversation_id/')
+  .get(passport.authenticate('jwt', {session:false}, participantsServices.getParticipantById))
+  .delete(passport.authenticate('jwt', {session:false}, participantsServices.deleteParticipantFromConversation))
